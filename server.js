a025b1bb27fccf0528c97597189a2f3c00844c5a
Wang SL <101174061+Guikong001@users.noreply.github.com>
2024-06-19 20:52:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (cluster.isMaster) {
     const axios = require('axios');  //用于处理流式相应
     const bodyParser = require('body-parser');
     const app = express();
-    const PORT = process.env.PORT || 7382;
+    const PORT = process.env.PORT;
 
     app.use(bodyParser.json());
 
